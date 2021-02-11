9f13f298f33311d83795ab0b2cf969d88dc08e89
dbongiov <61100159+dbongiov@users.noreply.github.com>
2021-02-11 10:16:15 -0500

    fixed lab 1

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,11 +1,37 @@
 /* Put your javascript in here */
 
+button.addEventListener('click', next);
+
 function next() {
-  alert("Forwards")
+    const a = document.querySelector('images');
+    {
+        a.scrollLeft;
+    }
 }
 
 function back() {
-    alert("Backwards")
-  }
+  alert('Backwards');
+}
+
+let i = i;
+for (let i of carousel.querySelectorAll('forward')) {
+  li.style.position = 'relative';
+  li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+  i++;
+}
+
+/* 
+
+after for loop
+
+>declare variables that are needed to create both functions (ie. the width/# of spaces you want to move)
+
+>select the forward button and apply function that will move forward x amount of space
+
+>select back button and apply function that will move back x amount of space
 
 
+
+how many images and elements are being displayed
+listen for onclick event and then apply a function
+moving position right or left a certain amount of spaces or width of page */
