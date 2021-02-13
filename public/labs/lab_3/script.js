3b90ba1b9c9641d4f83ccd66910c1ab5e4aa51fc
dbongiov <61100159+dbongiov@users.noreply.github.com>
2021-02-12 22:50:29 -0500

    comments

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -7,17 +7,13 @@ const list = document.querySelector('ul');
 const num = 7;
 
 function next() {
-  // shift right
   i -= width * count;
-  // can only shift the ribbbon for (total ribbon length - visible count) images
   i = Math.max(i, -width * (num - count));
   list.style.marginLeft = i + 'px';
 }
 
 function back() {
-  // shift left
   i += width * count;
-  // can't move to the left too much, end of images
   i = Math.min(i, 0)
   list.style.marginLeft = i + 'px';
 }
