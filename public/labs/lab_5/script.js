bd69a700faa6762fd934191f75744a5e67890fa3
dbongiov <61100159+dbongiov@users.noreply.github.com>
2021-03-12 20:37:13 -0500

    done for now

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,3 +1,5 @@
+
+
 /* eslint-disable max-len */
 function mapInit() {
   const mymap = L.map('mapid').setView([38.9897, -76.9378], 13);
@@ -23,28 +25,27 @@ async function dataHandler(mapFromLeaflet) {
   const data = await request.json();
 
   form.addEventListener('submit', async (event) => {
+
     event.preventDefault();
     console.log('form submitted');
 
     const filtered = data.filter((record) => record.zip.includes(search.value) && record.geocoded_column_1);
-    
     console.table(filtered);
 
     let count = 0;
     filtered.forEach((item) => {
-        
-    if(count < 5){
-      const longLat = item.geocoded_column_1.coordinates;
-      console.log('markerLongLat', longLat[0], longLat[1]);
-      const marker = L.marker([longLat[1], longLat[0]]).addTo(mapFromLeaflet);
+      if (count < 5) {
+        const longLat = item.geocoded_column_1.coordinates;
+        console.log('markerLongLat', longLat[0], longLat[1]);
+        const marker = L.marker([longLat[1], longLat[0]]).addTo(mapFromLeaflet);
         count++;
-    }
+      }
 
       const appendItem = document.createElement('li');
       appendItem.classList.add('block');
       appendItem.classList.add('list-item');
-      appendItem.innerHTML = '<div class = "list-header is-size-5">${item.name}</div><address class= "is-size-6">${item.adress_line_1}</address>';
-        
+      appendItem.innerHTML = '<div class = "list-header is-size-5">${item.name}</div><address class = "is-size-6">${item.adress_line_1}</address>';
+
       console.log(appendItem);
       displayMatches(filtered.slice(0, 5));
     });
@@ -54,18 +55,20 @@ async function dataHandler(mapFromLeaflet) {
 const suggestions = document.querySelector('.suggestions');
 
 function displayMatches(matchArray) {
-    console.log(this.value);
-    const html = matchArray.map(restaurants => {
+  console.log(this.value);
+  const html = matchArray.map(restaurants => {
     const regex = new RegExp(this.value, 'gi');
-      return  `
+    return `
         <li>
             <span class= "title">${restaurants.name}</span>
             <span class= "address">${restaurants.address_line_1}</span>
         </li>
         `;
-    }).join('');
-    suggestions.innerHTML = html;
+  }).join('');
+  suggestions.innerHTML = html;
 }
+
+
 
 async function windowActions() {
   const map = mapInit();
